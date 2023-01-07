c4557245c06e631ba9e636bc88cbb9a32787ac4f
unknown <judgepaulogebe3@gmail.com>
2023-01-07 21:38:21 +0100

    feat: create home screen

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function Home() {
     return(
-        <div className="d-flex align-items-center justify-content-center home-sec">
-            <div className="home--text">
+        <div className="d-flex flex-column align-items-center justify-content-center home-sec">
+            <div className="heading--text">
                 Quizzical
             </div>
+            <p>Welcome to Quizzical Nigeria's favorite free online trivia game.</p>
+            <button className="btn mt-3">Start quiz</button>
         </div>
     )
 }
