d3b319037f8ec1e8ccaff26004b0189fef223208
unknown <judgepaulogebe3@gmail.com>
2023-01-13 04:23:34 +0100

    feat: change button styling when on click
    I refactored the code to fit my needs I had to create a quizElements variable to render out the questions component, I fixed the function that changes the style of a button when it is clicked by setting the option selected to state and checking if that option is there and rendering it

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
 export default function Question(props) {
+    const optionElements = props.options[0].map(optionsObject => {
+        const classString = props.selected === optionsObject.option ? "btn mr-4 px-4 mt-2 pressed" : "btn mr-4 px-4 mt-2"
+            return (
+                <button 
+                onClick={(event) => props.setPressed(props.questionId, optionsObject.option)} 
+                key={optionsObject.id} 
+                className={classString}>
+                    {optionsObject.option}
+                </button>)
+        }
+    )
+    // return (<button 
+        //     onClick={(event) => setPressed(optionsObject.id)} 
+        //     key={optionsObject.id} 
+        //     className="btn mr-4 px-4 mt-2">
+        //         {decode(optionsObject.option)}
+        //     </button>)
     return (
         <>
             <p className="quiz--question">{props.question}</p>
-                {props.options}
+                {optionElements}
             <hr />
         </>
     )
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -9,51 +9,65 @@ export default function Quiz() {
 
     const [questionsData, setQuestionsData] = useState([]);
 
-useEffect(() => {
-        const fetchData = async () => {
-            const result = await fetch(apiURL)
-            result.json()
-                .then(json => json.results)
-                .then(dataRequest => setQuestionsData(dataRequest))
+    useEffect(() => {
+            const fetchData = async () => {
+                const result = await fetch(apiURL)
+                result.json()
+                    .then(json => json.results)
+                    .then(dataRequest => setQuestionsData(dataRequest))
+            }
+            fetchData()
+        }, [])
+
+
+    function setPressed(questionId, selectedOption) {
+        setQuiz(quizData => quizData.map( q => {
+            return questionId === q.id ?
+            ({...q, selected: selectedOption}) :
+            q
         }
-        fetchData()
-    }, [])
-
-
-useEffect(() => {
-    setQuiz(questionsData.map(question => {
-    function setPressed(id) {
+        ))
     }
-    let newObject = {question: "", options: [], answer: "", id: ""}
-    let optionsList = question.incorrect_answers
-    optionsList.push(question.correct_answer)
-    optionsList.sort(() => Math.random() - 0.5)
-    const optionElements = optionsList.map(option => {    
-        let optionsObject = {id: "", option: "", isPressed: false}
-        optionsObject.id = nanoid()
-        optionsObject.option = option
-        return (<button 
-            onClick={(event) => setPressed(optionsObject.id)} 
-            key={optionsObject.id} 
-            className="btn mr-4 px-4 mt-2">
-                {decode(optionsObject.option)}
-            </button>)
-    })
-    newObject.question = decode(question.question)
-    newObject.answer = question.correct_answer
-    newObject.id = nanoid()
-    return (<Question
-        key = {newObject.id}
-        question = {newObject.question}
-        options = {optionElements}
-        />)
-    }))
-}, [questionsData])
+    
+    let objectList = []
+    useEffect(() => {
+        setQuiz(questionsData.map(question => {
+        let newObject = {question: "", options: [], answer: "", id: ""}
+        let optionsList = question.incorrect_answers
+        optionsList.push(question.correct_answer)
+        optionsList.sort(() => Math.random() - 0.5)
+        let optionsObjectList = []
+        const options = optionsList.map(option => {
+            let optionsObject = {id: "", option: ""}
+            optionsObject.id = nanoid()
+            optionsObject.option = decode(option)
+            optionsObjectList.push(optionsObject)
+            return optionsObjectList
+        })
+        newObject.options = options
+        newObject.question = decode(question.question)
+        newObject.answer = question.correct_answer
+        newObject.id = nanoid()
+        return newObject
+        }))
+    }, [questionsData])
 
     
+    const quizElements = quiz.map(quizData => {
+        return (<Question
+            key = {quizData.id}
+            questionId = {quizData.id}
+            selected = {quizData.selected}
+            question = {quizData.question}
+            options = {quizData.options}
+            setPressed = {setPressed}
+        />)
+    }
+    )
     return(
         <div className="quiz-sec">
-            {quiz}
+            {/* {quiz} */}
+            {quizElements}
         </div>
     )
 }
