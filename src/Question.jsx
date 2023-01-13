1d6caa67e003e62ca8925f4b6c9000c0a88ff43f
unknown <judgepaulogebe3@gmail.com>
2023-01-13 05:04:47 +0100

    fix: make mobile friendly
    Made the options to have a default width on mobile

--- src/Question.jsx
+++ src/Question.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 export default function Question(props) {
     const optionElements = props.options[0].map(optionsObject => {
-        const classString = props.selected === optionsObject.option ? "btn mr-4 px-4 mt-2 pressed" : "btn mr-4 px-4 mt-2"
+        const classString = props.selected === optionsObject.option ? "btn mr-4 px-4 mt-2 option-btn pressed" : "btn mr-4 px-4 mt-2 option-btn"
             return (
-                <button 
-                onClick={(event) => props.setPressed(props.questionId, optionsObject.option)} 
-                key={optionsObject.id} 
-                className={classString}>
-                    {optionsObject.option}
-                </button>)
+                <div className="quiz--options mx-auto">
+                    <button 
+                    onClick={(event) => props.setPressed(props.questionId, optionsObject.option)} 
+                    key={optionsObject.id} 
+                    className={classString}>
+                        {optionsObject.option}
+                    </button>
+                </div>)
         }
     )
     // return (<button 
