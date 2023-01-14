3b71073e4afb399a5db06f240eecab9081f7cd1d
unknown <judgepaulogebe3@gmail.com>
2023-01-14 20:33:29 +0100

    fix: make fully responsive

--- src/Question.jsx
+++ src/Question.jsx
@@ -4,26 +4,21 @@ export default function Question(props) {
     const optionElements = props.options[0].map(optionsObject => {
         const classString = props.selected === optionsObject.option ? "btn mr-4 px-4 mt-2 option-btn pressed" : "btn mr-4 px-4 mt-2 option-btn"
             return (
-                <div className="quiz--options mx-auto">
                     <button 
                     onClick={(event) => props.setPressed(props.questionId, optionsObject.option)} 
                     key={optionsObject.id} 
                     className={classString}>
                         {optionsObject.option}
                     </button>
-                </div>)
+                )
         }
     )
-    // return (<button 
-        //     onClick={(event) => setPressed(optionsObject.id)} 
-        //     key={optionsObject.id} 
-        //     className="btn mr-4 px-4 mt-2">
-        //         {decode(optionsObject.option)}
-        //     </button>)
     return (
         <>
             <p className="quiz--question">{props.question}</p>
-                {optionElements}
+                <div className="quiz--options text-center-md">
+                    {optionElements}
+                </div>
             <hr />
         </>
     )
